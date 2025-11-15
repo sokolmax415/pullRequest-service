a0095a98428eed6db6cf0105bd3dc325478ca919
sokolmax415 <sokolmax415@gmail.com>
2025-11-16 01:34:15 +0300

    add pr_repository.go

---
+++ internal/repository/postgres/pr_repository.go
@@ -0,0 +1,129 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"pullrequest-service/internal/entity"
+
+	"github.com/Masterminds/squirrel"
+)
+
+type PostgresPRRepository struct {
+	db *sql.DB
+	sq squirrel.StatementBuilderType
+}
+
+func NewPostgresPRRepository(db *sql.DB) *PostgresPRRepository {
+	return &PostgresPRRepository{db: db, sq: squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)}
+
+}
+
+func (r *PostgresPRRepository) GetPRForAuthor(ctx context.Context, userId string) ([]entity.PullRequestShort, error) {
+	query, args, err := r.sq.Select("pr.pull_request_id", "pr.pull_request_name", "pr.author_id", "pr.status").From("pull_requests pr").
+		Join("pr_reviewers prr ON pr.pull_request_id = prr.pull_request_id").Where(squirrel.Eq{"prr.user_id": userId}).ToSql()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to build get PR author reviewer query: %w", err)
+	}
+
+	exec := executerFromContext(ctx, r.db)
+
+	rows, err := exec.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("exec PR for reviewer: %w", err)
+	}
+	defer rows.Close()
+
+	prList := make([]entity.PullRequestShort, 0)
+	for rows.Next() {
+		var pr entity.PullRequestShort
+
+		if err = rows.Scan(&pr.PullRequestID, &pr.PullRequestName, &pr.AuthorID, &pr.Status); err != nil {
+			return nil, fmt.Errorf("failed to scan: %w", err)
+		}
+		prList = append(prList, pr)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows error: %w", err)
+	}
+
+	return prList, nil
+}
+
+func (r *PostgresPRRepository) CreatePR(ctx context.Context, pr entity.PullRequestShort) error {
+	query, args, err := r.sq.Insert("pull_requests").Columns("pull_request_id", "pull_request_name", "author_id", "status").
+		Values(pr.PullRequestID, pr.PullRequestName, pr.AuthorID, pr.Status).ToSql()
+
+	if err != nil {
+		return fmt.Errorf("failed to build insert PR: %w", err)
+	}
+
+	exec := executerFromContext(ctx, r.db)
+
+	_, err = exec.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("exec insert PR: %w", err)
+	}
+
+	return nil
+}
+
+func (r *PostgresPRRepository) AddReviewerForPR(ctx context.Context, prId string, userId string) error {
+	query, args, err := r.sq.Insert("pr_reviewers").Columns("pull_request_id", "user_id").
+		Values(prId, userId).ToSql()
+
+	if err != nil {
+		return fmt.Errorf("failed to build insert pr_reviewer: %w", err)
+	}
+
+	exec := executerFromContext(ctx, r.db)
+
+	_, err = exec.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("exec insert pr_reviewer: %w", err)
+	}
+
+	return nil
+}
+
+func (r *PostgresPRRepository) MergePR(ctx context.Context, prId string) error {
+	query, args, err := r.sq.Update("pull_requests").Set("status", entity.MERGED).Where(squirrel.Eq{"pull_request_id": prId}).ToSql()
+
+	if err != nil {
+		return fmt.Errorf("failed to build update PR status: %w", err)
+	}
+
+	exec := executerFromContext(ctx, r.db)
+
+	_, err = exec.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("exec update PR status: %w", err)
+	}
+
+	return nil
+
+}
+
+func (r *PostgresPRRepository) IsReviewerForPR(ctx context.Context, prId string, userId string) (bool, error) {
+	query, args, err := r.sq.Select("1").From("pull_requests pr").
+		Join("pr_reviewers prr ON pr.pull_request_id = prr.pull_request_id").
+		Where(squirrel.Eq{"pr.pull_request_id": prId, "prr.user_id": userId}).ToSql()
+
+	if err != nil {
+		return false, fmt.Errorf("failed to build select reviewer exists query: %w", err)
+	}
+
+	exec := executerFromContext(ctx, r.db)
+
+	var dummy string
+	if err := exec.QueryRowContext(ctx, query, args...).Scan(&dummy); err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, fmt.Errorf("exec select reviewer exists query: %w", err)
+	}
+	return true, nil
+
+}
